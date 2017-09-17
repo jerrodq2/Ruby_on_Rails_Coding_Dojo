edcd43ce050d424659917d969b78dc1f4970c2de
Jerrod Quintana <jerrodq2@yahoo.com>
2017-09-17 12:13:59 -0500

    I added onto the word doc model_basics_validations_and_callbacks, so that I could add an example for belongs_to relationships that have the foreign key named differently than the model. Also added onto rails_console_query_examples_and_basics to list model_basics as a resource

--- full_rails/belt_review_professional_network/app/models/invitation.rb
+++ full_rails/belt_review_professional_network/app/models/invitation.rb
@@ -1,4 +1,7 @@
 class Invitation < ActiveRecord::Base
-  belongs_to :reciever, :class_name => 'User'
+  belongs_to :reciever, class_name: 'User' # I can use the ":class_name =>" or the "class_name:" format, both work.
   belongs_to :sender, :class_name => 'User'
+
+  # I have to use class_name because the foreign_key doesn't match the name of the model, User in this case. The columns are "reciever_id" and "sender_id" in this model, if either of these was named user_id, I could just put "belongs_to :user", but since they are named different, I have to tell rails which Model they are for. I have to use "class_name:" for a belongs_to, it basically says, this column is reciever_id but it actually is for the User model
+
 end
